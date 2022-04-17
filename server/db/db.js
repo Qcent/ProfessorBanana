3572a3507c1005827c76a9e0c499e34a33d2a442
Dave Quinn <dquinn8@cogeco.ca>
2022-04-17 16:11:03 -0400

    added ssl requirement to db connection for deployment

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,16 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-    logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/messenger',
+  {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see new error
+        rejectUnauthorized: false, // This line will fix new error
+      },
+    },
+  }
+);
 
 module.exports = db;
