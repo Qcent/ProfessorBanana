527f21e6e3928b380a97d866f21c48c6ab3425a1
Dave Quinn <dquinn8@cogeco.ca>
2022-04-17 12:00:20 -0400

    created method to order users by their recent chat activity for group chat participant sorting

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -34,17 +34,53 @@ const Chat = ({
 }) => {
   const classes = useStyles();
   const { otherMemberIds } = conversation;
+  let otherUser = conversation.members[otherMemberIds[0]];
 
-  // find the most recent other user that left a message
-  let i = conversation.messages.length - 1;
-  while (i >= 0 && conversation.messages[i].senderId === user.id) {
-    i--;
+  const sortUsersByRecentActivity = (memberIds) => {
+    const { messages } = conversation;
+
+    let ordered = [];
+    let i = messages.length - 1;
+    while (i >= 0) {
+      const { senderId } = messages[i];
+      if (senderId !== user.id && !ordered.includes(senderId)) {
+        ordered.push(senderId);
+        const idx = memberIds.indexOf(senderId);
+        if (idx > -1) {
+          memberIds.splice(idx, 1);
+        }
+        console.log(`Sender ${senderId} found and removed from members`);
+        console.log(memberIds);
+      }
+      i--;
+    }
+    if (memberIds.length) {
+      ordered.push(...memberIds);
+    }
+    return ordered;
+  };
+
+  if (otherMemberIds.length > 1) {
+    // find the most recent other user that left a message
+
+    console.log(
+      otherMemberIds,
+      'lets see how they sort:: ',
+      sortUsersByRecentActivity([...otherMemberIds])
+    );
+
+    let i = conversation.messages.length - 1;
+    while (i >= 0 && conversation.messages[i].senderId === user.id) {
+      i--;
+    }
+    otherUser =
+      i >= 0
+        ? conversation.members[conversation.messages[i].senderId] ||
+          conversation.members[otherMemberIds[0]]
+        : conversation.members[otherMemberIds[0]];
+  } else {
+    otherUser = conversation.members[otherMemberIds[0]];
   }
-  const otherUser =
-    i >= 0
-      ? conversation.members[conversation.messages[i].senderId] ||
-        conversation.members[otherMemberIds[0]]
-      : conversation.members[otherMemberIds[0]];
 
   const handleClick = async () => {
     await setActiveChat(conversation.id);
